02c92f958d793d3fc983aaa212291053ef577117
MyThirdRome <mythirdrome@gmail.com>
2025-02-07 20:24:40 +0100

    yt

--- setup.py
+++ setup.py
@@ -2,12 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='dou-blockchain',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(where='.', include=['src', 'src.*']),
     package_dir={'': '.'},
+    description='Decentralized Messaging and Reward Blockchain System',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/dou-blockchain',
     install_requires=[
+        'socket',
+        'json',
+        'logging',
+        'threading',
         'cryptography',
         'pytest',
         'mypy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,14 @@
 # DOU Blockchain main package
+
+"""
+DOU Blockchain Package Initialization
+"""
+
+# Package version
+__version__ = "0.1.0"
+
+# Import key components for easy access
+from .network.validator import ValidatorNode
+from .blockchain.core import DOUBlockchain
+from .messaging.system import DOUMessaging
+from .rewards.system import DOURewardSystem
